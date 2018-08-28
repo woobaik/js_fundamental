61ddc487b108491623d4be718bf536d01c76b64c
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-08-28 15:08:38 -0700

    Lecture 61

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+var haram = {
+  name: 'haram',
+  yearOfBirth: 1991,
+  gender: 'Female'
+}
+
+var Person = function(name, yearOfBirth,gender) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.gender = gender;
+}
+// instanciation.
+var haram = new Person('haram', 1991, 'Female');
