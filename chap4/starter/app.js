53c21f4177bb5d406277022da8b8dd7e818db8f8
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-08-27 19:19:09 -0700

    set up fundamental values with js

--- chap4/starter/app.js
+++ chap4/starter/app.js
@@ -8,3 +8,24 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let dice, currentScore, totalScore, newBtn, playerNumber;
+
+dice = Math.floor(Math.random() * 6) + 1;
+currentScore = [ 0, 0 ];
+totalScore = [0,0];
+playerNumber = 0;
+
+
+/***************************************************
+  ========when click a NEW GAME button==========
+
+  L reset currentScore to 0.
+  L reset totalScore to 0.
+  L hide the dice image.
+  L
+***************************************************/
+
+var currentScoreman = document.querySelector('#current-'+ playerNumber).textContent = dice;
+
+var score = document.querySelector('#score-'+ playerNumber).textContent =  totalScore[playerNumber];
