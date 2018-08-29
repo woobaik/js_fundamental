6177b0a2edb2ef73e5a08b43c526383db831c10b
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-08-28 19:37:31 -0700

    Finish lecture 64

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-var haram = {
-  name: 'haram',
-  yearOfBirth: 1991,
-  gender: 'Female'
-}
+// var haram = {
+//   name: 'haram',
+//   yearOfBirth: 1991,
+//   gender: 'Female'
+// }
+//
+// var Person = function(name, yearOfBirth,gender) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.gender = gender;
+// }
+// // instanciation.
+// var haram = new Person('haram', 1991, 'Female');
+//
+// Person.prototype.calculateAge =
+//   function() {
+//     console.log(2018 - this.yearOfBirth);
+//   }
+//
+// haram.calculateAge()
+// Person.prototype.lastName = "Kim"
 
-var Person = function(name, yearOfBirth,gender) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.gender = gender;
-}
-// instanciation.
-var haram = new Person('haram', 1991, 'Female');
+
+// Object.create
+
+var personProto = {
+  calculateAge: function() {
+    console.log(2018-this.yearOfBirth);
+  }
+};
+
+var peter = Object.create(personProto,
+ {name: {value : "peter"},
+ yearOfBirth: {value: 1982},
+ gender: {value: "male"}}
+)
