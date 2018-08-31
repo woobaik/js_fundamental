748bbc936cd65ecde656d5f2421f555295ddcda0
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-08-30 23:05:05 -0700

    Learn how to use private. pulic method

--- chap6/app.js
+++ chap6/app.js
@@ -8,9 +8,23 @@ var budgetController = (function() {
 
   return {
     publicTest: function(b) {
-      console.log(add(b))
+      return add(b);
     }
   }
-
-
 })();
+
+
+var uiController = (function() {
+  // some code;
+})();
+
+var controller = (function(budgetCtrl, uiCtrl) {
+
+  var z = budgetController.publicTest(4);
+
+  return {
+    anotherPublicController: function() {
+      console.log(z);
+    }
+  }
+})(budgetController, uiController)
