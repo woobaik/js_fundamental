7d8a310a29bf62daef7f0263a8ad3f512642fd12
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-12 16:55:00 -0700

    Finish removeFromArray excercise

--- exercises/javascript-exercises/removeFromArray/removeFromArray.js
+++ exercises/javascript-exercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
+  let arguArray = Array.from(arguments)
 
+  for (let i = 1; i < arguArray.length; i++) {
+    if (arguArray[0].includes(arguArray[i])) {
+      arguArray[0].splice(arguArray[0].indexOf(arguArray[i]), 1)
+
+    }
+  }
+  return arguArray[0]
 }
 
 module.exports = removeFromArray
