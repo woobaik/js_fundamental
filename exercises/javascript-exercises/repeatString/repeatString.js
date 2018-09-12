b5e32933c365420173426228c7f69b785c0a6652
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-12 14:25:41 -0700

    Finish exercise2

--- exercises/javascript-exercises/repeatString/repeatString.js
+++ exercises/javascript-exercises/repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+  let result = ""
+  if ( num < 0 ) {
+    return "ERROR"
+  }
+  for (let i = 0; i < num; i++) {
+    result = result.concat(str);
+  }
+  return result
 }
 
 module.exports = repeatString
