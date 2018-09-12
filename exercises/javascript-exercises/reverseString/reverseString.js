f136b3ecc70dcd010afc0d021f08a66551fea53f
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-12 15:39:46 -0700

    Finish ReverseString excercise

--- exercises/javascript-exercises/reverseString/reverseString.js
+++ exercises/javascript-exercises/reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let result = []
+  let charArray = str.split("")
+  for(let i = 0; i < str.length; i++) {
+    let lastEle = charArray.pop()
+    result.push(lastEle)
+  }
+  return result.join("")
 }
 
 module.exports = reverseString
