7413bce19320e769f53bea8354c61a03311935fc
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-12 19:57:35 -0700

    Finish temp calculator

--- exercises/javascript-exercises/tempConversion/tempConversion.js
+++ exercises/javascript-exercises/tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahren) {
+  let result;
+  result = (fahren - 32) * 5/9;
+  return Math.round( result * 10 ) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(celci) {
+  let result;
+  result = celci * 9/5 + 32
+  return Math.round(result * 10 ) / 10
 }
 
 module.exports = {
