7b6a6b0eb217567b14d3b6ff204a3bc292f18378
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-12 19:30:03 -0700

    Finish sumAll

--- exercises/javascript-exercises/sumAll/sumAll.js
+++ exercises/javascript-exercises/sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  
+  if ( typeof(num1) !== "number" || typeof(num2) !== "number" ) {
+    return "ERROR"
+  }
+  else if (num1 < 0 || num2 < 0) {
+    return "ERROR"
+  }
+  else {
+    let smallNumber, bigNumber;
+    let sum = 0
+    // num1 < num2 ? (smallNumber = num1, bigNumber = num2) : (smallNumber = num2 && bigNumber = num1)
+
+    if (num1 < num2) {
+      smallNumber = num1
+      bigNumber = num2
+    } else {
+      smallNumber = num2
+      bigNumber = num1
+    }
+
+    for (let i = smallNumber; i <= bigNumber; i++) {
+      sum += i
+    }
+    return sum;
+  }
+
 
 }
 
