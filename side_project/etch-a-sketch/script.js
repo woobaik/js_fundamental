623c97ca3c1dd65711ae7723269320f0f29e160e
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-16 01:40:35 -0700

    defined grid

--- side_project/etch-a-sketch/script.js
+++ side_project/etch-a-sketch/script.js
@@ -1,12 +1,25 @@
 const container = document.querySelector('#container');
+let userInput = 4;
+const gridSize = 600 / userInput;
 
-let userInput = 16
+container.style.cssText = `grid-template-columns: repeat(${userInput}, 1fr)`
 
 
+for (let i =0; i < userInput*userInput; i++ ) {
+  const grid = document.createElement('div');
+  grid.style.width = gridSize + "px";
+  grid.style.height = gridSize + "px";
+  grid.classList.add('grid');
+  container.appendChild(grid);
 
-for (let i =0; i < userInput; i++ ) {
-  const div = document.createElement('div');
-  div.classList.add('grid');
-  container.appendChild(div);
+}
 
+
+const grids = document.querySelectorAll(".grid")
+  grids.forEach(grid) => {
+    grid.addEventListener('mouseover', color)
+  }
+
+function color() {
+  this.classList.add('colored')
 }
