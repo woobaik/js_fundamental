336ce23dedfbd3ceaf5f91d79ee16a4ec0082f9b
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-11-16 13:13:04 -0800

    Finish roll dice button

--- chap4/starter/app.js
+++ chap4/starter/app.js
@@ -8,3 +8,48 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var rollDiceBtn, diceBtn, activePlayer, current, total;
+
+activePlayer = 0
+current = 0
+rollDiceBtn = document.querySelector('.btn-roll')
+DiceBtn = document.querySelector('.dice')
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+// #initialize
+
+
+
+rollDiceBtn.addEventListener('click', function() {
+  // genenrate number
+  var randomNumber = Math.random() * 6;
+  var dice = Math.floor(randomNumber) + 1;
+
+
+  DiceBtn.src = 'dice-' + dice + '.png'
+
+  // check if dice is 1, if so, change activePlayer, else, add number to current.
+  if (dice !== 1) {
+    current += dice;
+    reflectCurrent()
+  }
+  else {
+    current = 0;
+    reflectCurrent()
+    toggleActivePlayer()
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    toggleActivePlayer()
+    // <div class="player-0-panel active">
+  }
+
+  // display current sum number to currentbox
+  function reflectCurrent() {
+    document.getElementById('current-'+ activePlayer).textContent = current;
+  }
+
+  // toggle active player status
+  function toggleActivePlayer() {
+    document.querySelector('.player-'+ activePlayer + '-panel').classList.toggle("active")
+  }
+})
