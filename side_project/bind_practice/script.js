7c2c5d8a321915b9329116adfaf3544e6ce6cffe
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-11-19 22:59:02 -0800

    Check where the inner function this binds

--- side_project/bind_practice/script.js
+++ side_project/bind_practice/script.js
@@ -1,23 +1,23 @@
-var result = 100;
-
-var myObj = {
-  result: 1,
-  myFnc1: function() {
-    this.result += 1;
-    console.log('This is from myFnc1 :' + this.result)
-
-    myFnc2 = function() {
-      this.result += 1;
-      console.log('This is from myFnc2 :' + this.result)
-
-      myFnc3 = function() {
-        this.result += 1;
-        console.log('This is from myFnc3 :' + this.result )
-      }
-      myFnc3()
-    }
-    myFnc2()
-  }
-}
-
-myObj.myFnc1()
+var result = 100
+// 
+// var myObj = {
+//   result: 1,
+//   myFnc1: function() {
+//     this.result += 1;
+//     console.log('This is from myFnc1 :' + this.result)
+//
+//     function fnc2() {
+//       this.result += 1
+//       console.log('This is from myFnc2 :' + this.result)
+//
+//       function fnc3() {
+//         this.result += 1
+//         console.log('This is from myFnc3 :' + this.result)
+//       }
+//       fnc3()
+//     }
+//     fnc2()
+//   }
+// }
+//
+// myObj.myFnc1()
