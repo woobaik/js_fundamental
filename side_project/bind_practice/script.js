9b9d5bed41a227aff0a38c33b6cd9fd181bdd5ee
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-11-19 23:10:22 -0800

    Used that for binding

--- side_project/bind_practice/script.js
+++ side_project/bind_practice/script.js
@@ -1,5 +1,7 @@
 var result = 100
-// 
+
+
+// ===THIS OBJ WILL RETURN RESULT OF 2, 101, 102
 // var myObj = {
 //   result: 1,
 //   myFnc1: function() {
@@ -21,3 +23,29 @@ var result = 100
 // }
 //
 // myObj.myFnc1()
+
+
+// THIS OBJ WILL RETURN WHAT WE EXPECTED (1,2,3)
+var result = 100;
+var myObj = {
+  result: 1,
+  myfnc1: function() {
+    var that = this
+    that.result += 1
+    console.log("This is from fnc1 " + that.result)
+
+    function fnc2() {
+      that.result += 1
+      console.log("This is from fnc2 " + that.result)
+
+      function fnc3(){
+        that.result += 1
+        console.log("This is from fnc3 " + that.result)
+      }
+      fnc3()
+    }
+    fnc2()
+  }
+}
+
+myObj.myfnc1()
