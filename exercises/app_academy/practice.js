3cb5b4e172ae8b1481a7162a5d4172290b40d0c6
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-11-20 21:05:15 -0800

    Finish parse1

---
+++ exercises/app_academy/practice.js
@@ -0,0 +1,100 @@
+// function mysteryScoping1() {
+//   var x = 'out of block';
+//   if (true) {
+//     var x = 'in block';
+//     console.log(x);
+//   }
+//   console.log(x)
+// }
+//
+// mysteryScoping1()
+// console.log('==================')
+//
+//
+// function mysteryScoping2() {
+//   const x = 'out of block';
+//   if (true) {
+//     const x = 'in block';
+//     console.log(x)
+//   }
+//   console.log(x)
+// }
+//
+// mysteryScoping2()
+// console.log('==================')
+//
+//
+// function mysteryScoping3() {
+//   let x = 'out of block';
+//   if (true) {
+//     let x = 'in block';
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
+//
+//
+// mysteryScoping3()
+// console.log('==================')
+//
+//
+// function mysteryScoping4() {
+//   const x = 'out of block';
+//   if (true) {
+//     var x = 'in block';
+//     console.log(x);
+//   }
+//   console.log(x)
+// }
+//
+//
+// // mysteryScoping4()
+//
+//
+// function mysteryScoping5() {
+//   let x = 'out of block';
+//   let x = 'out of block again';
+//   if (true) {
+//     let x = 'in block';
+//     console.log(x);
+//   }
+//
+//   console.log(x);
+// }
+//
+// mysteryScoping5()
+//
+//
+// let madLib = (verb, adjective, noun) => {
+//   console.log(`We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}`)
+// }
+//
+//
+// madLib('make', 'best', 'taco')
+
+//
+// let isSubstring = (searchString, subString) => {
+//     if (searchString.includes(subString)) {
+//       console.log('true')
+//     }
+//     else {
+//     console.log(false)
+//     }
+// }
+// isSubstring("time to program", "time")
+// isSubstring("Jump for joy", "joys")
+
+
+
+// let fizzBuzz = (arr) => {
+//   let result = []
+//   for (let i = 0; i < arr.length; i++) {
+//     if (( arr[i] % 3 === 0 || arr[i] % 5 === 0 ) && arr[i] % 15 !== 0 ) {
+//       result.push(arr[i]);
+//     }
+//   }
+//   console.log(result);
+// }
+//
+//
+// fizzBuzz([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
