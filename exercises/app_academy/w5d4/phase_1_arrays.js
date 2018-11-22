ddac0a468e0ac6d44d9b9619a70e7113f4507a8a
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-11-21 19:53:14 -0800

    Finish TRANSPOSEgit add .

--- exercises/app_academy/w5d4/phase_1_arrays.js
+++ exercises/app_academy/w5d4/phase_1_arrays.js
@@ -5,34 +5,49 @@
 // Array#transpose - where we have a two-dimensional array representing a matrix. returns the transpose
 // should not mutate the original array
 
-
-Array.prototype.uniq = function() {
-  let result = []
-  for (let i = 0; i < this.length; i++) {
-    if (!(result.includes(this[i]))) {
-      result.push(this[i])
-    }
-  }
-  console.log(result)
-}
 //
-// let zico = [1,2,3,4,5,6,1,4,2,1,6,7]
+// Array.prototype.uniq = function() {
+//   let result = []
+//   for (let i = 0; i < this.length; i++) {
+//     if (!(result.includes(this[i]))) {
+//       result.push(this[i])
+//     }
+//   }
+//   console.log(result)
+// }
+// //
+// // let zico = [1,2,3,4,5,6,1,4,2,1,6,7]
+// //
+// // zico.uniq()
+// // console.log(zico)
 //
-// zico.uniq()
-// console.log(zico)
+//
+// Array.prototype.twoSum = function() {
+//   let result = []
+//   for (let i = 0; i < this.length - 1; i++) {
+//     for (let j = 1; j < this.length; j ++) {
+//       if (this[i] + this[j] === 0 ) {
+//         result.push([i,j])
+//       }
+//     }
+//   }
+//   console.log(result)
+// }
+//
+// let tryndamir = [1,2,3,4,5,6,-1,-4,0,-3,4,0 ];
+// console.log(tryndamir.twoSum());
+
+// Array.prototype.transpose = function() {
+//   let new_arr = [];
+//
+//   for (let i = 0; i < this.length; i++) {
+//     new_arr.push([])
+//     for (let j = 0; j < this.length; j++) {
+//       new_arr[j].push(this[i][j])
+//     }
+//   }
+//   console.log(new_arr)
+// }
 
 
-Array.prototype.twoSum = function() {
-  let result = []
-  for (let i = 0; i < this.length - 1; i++) {
-    for (let j = 1; j < this.length; j ++) {
-      if (this[i] + this[j] === 0 ) {
-        result.push([i,j])
-      }
-    }
-  }
-  console.log(result)
-}
-
-let tryndamir = [1,2,3,4,5,6,-1,-4,0,-3,4,0 ];
-console.log(tryndamir.twoSum());
+array[0].map((col, i => array.map(row => row[i]))
