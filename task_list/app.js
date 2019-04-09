7fb6992e8bae5c3bcad110ec4a6cb017b8a9f76c
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-04-09 13:07:47 -0700

    Finish part 1

--- task_list/app.js
+++ task_list/app.js
@@ -1,31 +1,34 @@
-//Define UI selector
+// define UI selector
+
 const addForm = document.querySelector('.task-form');
 const addInput = document.querySelector('#task');
-const taskList = document.querySelector('.collection');
+const taskList = document.querySelector('.collection')
 
-loadAddEventListeners();
 
-function loadAddEventListeners() {
-    addForm.addEventListener('submit', addTask)
+loadEventListeners();
+
+
+function loadEventListeners() {
+    addForm.addEventListener('submit', addTask);
 }
 
 function addTask(e) {
-    if (addInput.value === '') {
-        alert('Add new Task Please');
-    }
-
+    //create li element
     const li = document.createElement('li');
+
+    // add class to li
     li.className = 'collection-item';
-    
     li.appendChild(document.createTextNode(addInput.value));
 
     const deleteLink = document.createElement('a');
     deleteLink.className = 'delete-item secondary-content';
-    deleteLink.innerHTML = "<i class='fas fa-times'></i>"
+    deleteLink.innerHTML = "<i class='fas fa-times'></i>";
+
     li.appendChild(deleteLink);
 
-    taskList.appendChild(li)
-    
+    taskList.appendChild(li);
+
+
     addInput.value = ''
     e.preventDefault();
 }
