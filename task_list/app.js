4059fe5d847f710b91f05e8ea27b80d41298c93d
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-04-09 16:32:14 -0700

    Add clear tasks, and delete node

--- task_list/app.js
+++ task_list/app.js
@@ -1,24 +1,22 @@
 const addForm = document.querySelector('.task-form');
 const addInput = document.querySelector('#task');
 const taskList = document.querySelector('.collection');
+const clearItemBtn = document.querySelector('.clear-task');
 
 loadEventListener();
 
 function loadEventListener() {
     addForm.addEventListener('submit', addList);
-    
+    clearItemBtn.addEventListener('click', clearItem);
+    taskList.addEventListener('click', deleteItem);
 }
 
 function addList(e) {
     if (!(addInput.value)) {
         alert('Please Add New Task');
     };
-
-
     const li = document.createElement('li');
-
     li.className = 'collection-item';
-
     li.appendChild(document.createTextNode(addInput.value));
     
     const deleteLink = document.createElement('a');
@@ -34,3 +32,19 @@ function addList(e) {
     e.preventDefault()
 }
 
+function clearItem(e) { 
+    console.log(e.target.parentElement);
+    while(taskList.firstChild ) {
+        taskList.removeChild(taskList.firstChild);
+    }
+    
+    e.preventDefault();
+}
+
+
+function deleteItem(e) {
+    if (e.target.className === "fas fa-times") {
+        e.target.parentElement.parentElement.remove()
+    }
+    
+}
