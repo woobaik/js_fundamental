ec248f52285a4b6742ab8b9278ace75015f05698
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-04-09 15:06:36 -0700

    Refine item page

--- task_list/app.js
+++ task_list/app.js
@@ -1,25 +1,26 @@
-// define UI selector
-
 const addForm = document.querySelector('.task-form');
 const addInput = document.querySelector('#task');
-const taskList = document.querySelector('.collection')
+const taskList = document.querySelector('.collection');
 
+loadEventListener();
 
-loadEventListeners();
-
-
-function loadEventListeners() {
-    addForm.addEventListener('submit', addTask);
+function loadEventListener() {
+    addForm.addEventListener('submit', addList);
+    
 }
 
-function addTask(e) {
-    //create li element
+function addList(e) {
+    if (!(addInput.value)) {
+        alert('Please Add New Task');
+    };
+
+
     const li = document.createElement('li');
 
-    // add class to li
     li.className = 'collection-item';
-    li.appendChild(document.createTextNode(addInput.value));
 
+    li.appendChild(document.createTextNode(addInput.value));
+    
     const deleteLink = document.createElement('a');
     deleteLink.className = 'delete-item secondary-content';
     deleteLink.innerHTML = "<i class='fas fa-times'></i>";
@@ -28,7 +29,8 @@ function addTask(e) {
 
     taskList.appendChild(li);
 
-
     addInput.value = ''
-    e.preventDefault();
+
+    e.preventDefault()
 }
+
