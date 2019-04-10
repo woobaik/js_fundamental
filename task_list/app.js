c0fc5b6ba85bb3db759d29d840ed92aed9504277
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-04-09 17:37:54 -0700

    Finish chapter 1

--- task_list/app.js
+++ task_list/app.js
@@ -3,11 +3,13 @@ const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-task');
 
+
 loadEventListeners()
 
 function loadEventListeners() {
     taskForm.addEventListener('submit', addList);
     clearBtn.addEventListener('click', clearList);
+    taskList.addEventListener('click', deleteItem);
 }
 
 function addList(e) {
@@ -33,6 +35,14 @@ function clearList(e) {
     while (taskList.firstElementChild) {
         taskList.firstElementChild.remove()
     }
-    
+
     e.preventDefault();
 };
+
+function deleteItem(e) {
+    if (e.target.className === 'fas fa-times') {
+        e.target.parentElement.parentElement.remove();
+    }
+    
+    e.preventDefault();
+}
