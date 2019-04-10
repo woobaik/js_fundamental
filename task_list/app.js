c303716fc5c3dad93bb71d07df045f19a6908e23
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-04-09 17:11:56 -0700

    Finish adding function feature

--- task_list/app.js
+++ task_list/app.js
@@ -1,50 +1,28 @@
-const addForm = document.querySelector('.task-form');
-const addInput = document.querySelector('#task');
+const taskForm = document.querySelector('.task-form');
+const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('.collection');
-const clearItemBtn = document.querySelector('.clear-task');
 
-loadEventListener();
+loadEventListeners()
 
-function loadEventListener() {
-    addForm.addEventListener('submit', addList);
-    clearItemBtn.addEventListener('click', clearItem);
-    taskList.addEventListener('click', deleteItem);
+function loadEventListeners() {
+    taskForm.addEventListener('submit', addList);
 }
 
 function addList(e) {
-    if (!(addInput.value)) {
-        alert('Please Add New Task');
-    };
     const li = document.createElement('li');
-    li.className = 'collection-item';
-    li.appendChild(document.createTextNode(addInput.value));
-    
-    const deleteLink = document.createElement('a');
-    deleteLink.className = 'delete-item secondary-content';
-    deleteLink.innerHTML = "<i class='fas fa-times'></i>";
 
-    li.appendChild(deleteLink);
+    li.className = "collection-item"
+
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    const deleteBtn = document.createElement('a');
+
+    deleteBtn.className = "delete-item secondary-content";
+    deleteBtn.innerHTML = "<i class='fas fa-times'></i>";
+
+    li.appendChild(deleteBtn);
 
     taskList.appendChild(li);
 
-    addInput.value = ''
-
-    e.preventDefault()
-}
-
-function clearItem(e) { 
-    console.log(e.target.parentElement);
-    while(taskList.firstChild ) {
-        taskList.removeChild(taskList.firstChild);
-    }
-    
     e.preventDefault();
-}
-
-
-function deleteItem(e) {
-    if (e.target.className === "fas fa-times") {
-        e.target.parentElement.parentElement.remove()
-    }
-    
 }
