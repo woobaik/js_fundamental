0f5dafb8e81cf4f83949f9fc337724fc5d0a07d5
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-08-12 10:36:12 -0700

    Add delete function

--- card_task/app.js
+++ card_task/app.js
@@ -1,10 +1,18 @@
 const submitBtn = document.querySelector('.submit-btn');
 const textInput = document.querySelector('.text-input');
-const listArea = document.querySelector('.todo-list')
-console.log(listArea);
+const listArea = document.querySelector('.todo-list');
+const deleteBtn = document.querySelector('.delete-btn');
+console.log(deleteBtn)
+
+eventLoader()
+
+function eventLoader() {
+    submitBtn.addEventListener('click', addList);
+    listArea.addEventListener('click', deleteList)
+}
 
 
-submitBtn.addEventListener('click', addList);
+    
 
 
 function addList() {
@@ -15,7 +23,7 @@ function addList() {
         const textValue = document.createTextNode(textInput.value)
         const linkTag = document.createElement('a')
         linkTag.className = 'delete-item secondary-content'
-        linkTag.innerHTML = '<i class="fas fa-times"></i>'  
+        linkTag.innerHTML = '<i class="fas fa-times delete-btn"></i>'  
         li.className = 'collection-item'
         li.appendChild(textValue);
         li.appendChild(linkTag)
@@ -23,6 +31,12 @@ function addList() {
         listArea.appendChild(li)
 
         textInput.value = ''
-        console.log(li)
+        console.log(deleteBtn)
+    }
+}
+
+function deleteList(e) {
+    if (e.target.classList.contains('delete-btn')) {
+        e.target.parentElement.parentElement.style.display = 'none'
     }
 }
