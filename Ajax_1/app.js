27d870e4b1cd6f1ce1ba282b7ed13a70eccf9852
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-08-13 09:20:53 -0700

    Finish customers fetch

--- Ajax_1/app.js
+++ Ajax_1/app.js
@@ -1,29 +1,60 @@
 const customerBtn = document.querySelector('#customer-btn');
 const customersBtn = document.querySelector('#customers-btn');
-
+let requested = false
 
 customerBtn.addEventListener('click', function() {
-    
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', 'customer.json', true)
+    if (requested === false) {
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', 'customer.json', true)
 
-    xhr.onload = function() {
-        if( this.status == 200) {
-            console.log(this.responseText)
-            const container = document.querySelector('.container')
-            const customer = JSON.parse(this.responseText)
-            const customersList = document.querySelector('.customers-list')
-            const div = document.createElement('div')
-            div.innerHTML = `
+        xhr.onload = function () {
+            if (this.status == 200) {
+                console.log(this.responseText)
+                const container = document.querySelector('.container')
+                const customer = JSON.parse(this.responseText)
+                const customersList = document.querySelector('.customers-list')
+                const div = document.createElement('div')
+                div.innerHTML = `
                 id : ${customer.id}, <br>
                 name: ${customer.name}, <br>
                 address: ${customer.address}
             `
-            container.insertBefore(div, customersList)
-            console.log(customersList)
-        }
-    }
+                container.insertBefore(div, customersList)
 
-    xhr.send()    
+            }
+        }
+        requested = true;
+        xhr.send()    
+    } 
+    
 })
 
+customersBtn.addEventListener('click', function() {
+
+    const xhr = new XMLHttpRequest()
+
+    xhr.open('GET', 'customers.json', true)
+
+    xhr.onload = function() {
+        if (this.status === 200) {
+            
+            const customersArea = document.querySelector('.customers-list')
+            let customersList = ''
+            const customers = JSON.parse(this.responseText)
+            customers.forEach(function(customer) {
+                customersList += `
+                    <ul>
+                        <li>${customer.id}</li>
+                        <li>${customer.name}</li>
+                        <li>${customer.address}</li>
+                    </ul>
+                `
+            })
+            customersArea.innerHTML =customersList
+            
+        }
+
+    }
+
+    xhr.send()
+})
