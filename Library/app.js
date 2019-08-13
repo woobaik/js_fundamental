68cce78c58c8bd9a26116c725be8b641e3fa9998
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-08-12 17:40:54 -0700

    Create boiler plate

---
+++ Library/app.js
@@ -0,0 +1,15 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title
+        this.author = author
+        this.isbn = isbn
+    }
+}
+
+const book = new Book('rosuarmf', 'jujangso',123123)
+console.log(book)
+
+class UI {
+    
+}
+
